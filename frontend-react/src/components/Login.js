e55ba3f94cdda6be41bdec566c06d3186d46c7e6
Mondifik <mondifik@mail.ru>
2025-09-04 23:20:58 +0300

    Add debug logs for API_URL

--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -16,7 +16,7 @@ function Login({ onLoginSuccess }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const API_URL = 'http://127.0.0.1:8000';
+    const API_URL = process.env.REACT_APP_API_URL;
 
     const handleSubmit = async (event) => {
         event.preventDefault();
